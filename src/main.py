3967a3c86c4d36b2a1022496209ee783133dc6d5
Martinelllo <hellmann.martin@gmx.net>
2023-09-24 22:43:59 +0200

    Device Authentifizierung teilweise implementiert

--- src/main.py
+++ src/main.py
@@ -6,23 +6,46 @@ import time
 from datetime import datetime
 import requests
 
-device=2
 
-def sendSensorValue(id, value):
+def sendSensorValue(id, value, headers):
    body = {
-      "deviceId": device,
       "sensorId": id,
       "value": round(value, 1)
    }
-
    try:
-      response = requests.post(url + '/sensor-value', json=body, headers=headers)
-      print(response.text)
+      response = requests.post(URL + '/sensor-value', json=body, headers=headers)
+      print('Sensor wert erfolgreich gesendet. Id' + id)
    except:
       print('request error')
 
 
 if __name__ == "__main__":
+
+   URL = "https://smarthome-api.hellmannweb.de" # server
+   # URL = "http://192.168.178.20"
+
+   # DEVICE_NAME='SensorPi'
+   # DEVICE_PASSWORD='awdawd123'
+
+   headers = {"Content-Type": "application/json; charset=utf-8"}
+
+   # body = {
+   #    "deviceName": DEVICE_NAME,
+   #    "devicePassword": DEVICE_PASSWORD,
+   # }
+   # try:
+   #    response = requests.post(URL + '/device-auth', json=body, headers=headers)
+   #    token = response.text
+   #    print(token)
+   #    headers = {
+   #       "Content-Type": "application/json; charset=utf-8",
+   #       "Authorization": "Bearer "+token,
+   #    }
+   # except:
+   #    print('request error')
+   #    exit('Unauthorized')
+
+   INTERVAL = 60
 
    i2c = busio.I2C(board.SCL, board.SDA)
    bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
@@ -34,63 +57,21 @@ if __name__ == "__main__":
    bme280.mode = adafruit_bme280.MODE_NORMAL
    time.sleep(1) # wait until gpiod starts
 
-   INTERVAL = 1
-
    while True:
 
-      print("Temperature: %0.1f C" % bme280.temperature)
-      # print("Humidity: %0.1f %%" % bme280.humidity)
-      print("relative Humidity: %0.1f %%" % bme280.relative_humidity)
-      print("absolute Pressure: %0.1f hPa" % bme280.pressure)
-      # print("Altitude = %0.2f meters" % bme280.altitude)
-      print("-------------------------------------")
+      # print("Temperature: %0.1f C" % bme280.temperature)
+      # # print("Humidity: %0.1f %%" % bme280.humidity)
+      # print("relative Humidity: %0.1f %%" % bme280.relative_humidity)
+      # print("absolute Pressure: %0.1f hPa" % bme280.pressure)
+      # # print("Altitude = %0.2f meters" % bme280.altitude)
+      # print("-------------------------------------")
 
-
-      url = "https://smarthome-api.hellmannweb.de" # server
-      # url = "http://192.168.178.20" # server
-
-      headers = {
-         "Content-Type": "application/json; charset=utf-8",
-      }
+      sendSensorValue(3, bme280.temperature, headers)
+      sendSensorValue(4, bme280.relative_humidity, headers)
+      sendSensorValue(5, bme280.pressure, headers)
 
       # utcTimeStamp = datetime.utcnow().timestamp()
       # currentTime = datetime.now()
 
-      # temperatureData = {
-      #    "sensorId": 3,
-      #    "value": round(bme280.temperature, 1)
-      # }
-
-      # humidityData  = {
-      #    "sensorId": 4,
-      #    "value": round(bme280.humidity, 1)
-      # }
-
-      # humidityData  = {
-      #    "sensorId": 5,
-      #    "value": round(bme280.pressure, 1)
-      # }
-
-      # try:
-      #    response = requests.post(url + '/sensor-value', json=temperatureData, headers=headers)
-      #    print(response.text)
-      # except:
-      #    print('request error')
-
-      # try:
-      #    response = requests.post(url + '/sensor-value', json=humidityData, headers=headers)
-      #    print(response.text)
-      # except:
-      #    print('request error')
-
-      # try:
-      #    response = requests.post(url + '/sensor-value', json=humidityData, headers=headers)
-      #    print(response.text)
-      # except:
-      #    print('request error')
-
 
       time.sleep(INTERVAL)  # Overall INTERVAL second polling.
-
-   s.cancel()
-   pi.stop()
